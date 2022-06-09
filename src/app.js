29811b1dfdc6a38e2fe87dd2febd1b1906d8e2bb
Hanksito <barjabalseiro@gmail.com>
2022-06-09 08:25:25 +0000

    generador de escusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#escusas").innerHTML = Escusas();
+
+  function Escusas() {
+    let pronoun = ["A", "The"];
+    let subject = [
+      "jogger",
+      "uncle",
+      "dog",
+      "driver",
+      "a stranger",
+      "neighbour"
+    ];
+    let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+    let possetion = ["homework ", "food", "car", "shoe", "Secret place"];
+    let where = [
+      "on the street",
+      "in my house",
+      "in my driveway",
+      "in my dreams"
+    ];
+
+    let pronounramdon = Math.floor(Math.random() * pronoun.length);
+    let subjectramdon = Math.floor(Math.random() * subject.length);
+    let actionramdon = Math.floor(Math.random() * action.length);
+    let possetionramdon = Math.floor(Math.random() * possetion.length);
+    let whereramdon = Math.floor(Math.random() * where.length);
+
+    return (
+      pronoun[pronounramdon] +
+      " " +
+      subject[subjectramdon] +
+      " " +
+      action[actionramdon] +
+      " " +
+      possetion[possetionramdon] +
+      " " +
+      where[whereramdon]
+    );
+  }
 };
